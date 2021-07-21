47bdba7c894efd78432ecc2521b2a36fd1eef26d
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-21 12:54:13 +0200

    Making a counter with the state in store file

--- 3-state/src/App.vue
+++ 3-state/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Ejemplo de Vuex básico</h1>
+    <Counter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Counter from './components/Counter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ 3-state/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>El valor del contador es: {{this.$store.state.count}}</p>
+    <button @click="increment">Sumar</button>
+    <button @click="decrement">Restar</button>
+  </div>
+</template>
+
+<script>
+
+  import {store} from '../store/store'
+
+  export default {
+    name: 'Counter',
+    store,
+    methods: {
+      increment() {
+        this.$store.commit('increment')
+      },
+      decrement() {
+        this.$store.commit('decrement')
+      }
+}
+  }
+</script>
+
+<style scoped>
+
+</style>
