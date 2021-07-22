e2b4a5a4a03b3c554fc92305d7d2e81d946d0f91
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-22 13:39:22 +0200

    Add button to Blog to back Start using router.push

--- 4-router/src/components/Blog.vue
+++ 4-router/src/components/Blog.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
       <h2>Blog</h2>
+      <button @click="backToStart">Volver a Inicio</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Blog'
+    name: 'Blog',
+    methods:{
+      backToStart(){
+        this.$router.push('/')
+      }
+    }
   }
 </script>
 
