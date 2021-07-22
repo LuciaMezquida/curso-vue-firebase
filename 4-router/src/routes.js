407874fd76d01e63eadeb9f9d05f5eef633bcd59
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-22 12:33:24 +0200

    Create routes and add them to main.js

---
+++ 4-router/src/routes.js
@@ -0,0 +1,7 @@
+import Start from "./components/Start";
+import Blog from "./components/Blog";
+
+export const routes = [
+  { path: "/", component: Start },
+  { path: "/blog", component: Blog },
+];
